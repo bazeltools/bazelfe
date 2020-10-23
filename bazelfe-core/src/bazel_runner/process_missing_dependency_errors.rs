f9c150a869367868f6e5189a0e480ec73ab24606
Ian O'Connell <ianoc@ianoc.net>
2020-10-23 14:33:21 -0600

    Add more test

--- bazelfe-core/src/bazel_runner/process_missing_dependency_errors.rs
+++ bazelfe-core/src/bazel_runner/process_missing_dependency_errors.rs
@@ -33,6 +33,7 @@ use log;
           };
       }
 
+      println!("Uh.. huh {:?}", label);
       if let Some(forbidden_targets) = target_kind.as_ref().and_then(|nme| FORBIDDEN_TARGETS_BY_TYPE.get(nme)) {
           if forbidden_targets.contains(label) {
               return false;
@@ -174,8 +175,9 @@ pub async fn process_missing_dependency_errors<T: Buildozer + Clone + Send + Syn
                     index_table.get_from_suffix( &suffix.suffix).await
                 }
             };
-            debug!("Candidates for class name: {:#?} : {:#?}", req, candidates);
             for target_entry in &candidates.read_iter().await {
+                debug!("Processing candidate for class name: {:#?} : {:#?}", req, target_entry);
+
                 if !ignore_dep_references.contains(&target_entry.target)
                     && is_potentially_valid_target(&action_failed_error_info.target_kind,&target_entry.target)
                 {
@@ -217,9 +219,11 @@ pub async fn process_missing_dependency_errors<T: Buildozer + Clone + Send + Syn
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
+    use std::{sync::Arc, path::PathBuf};
 
-        use crate::error_extraction::{ClassImportRequest, ClassSuffixMatch};
+            use tokio::sync::Mutex;
+
+use crate::{buildozer_driver::ExecuteResultError, error_extraction::{ClassImportRequest, ClassSuffixMatch}};
 
 use super::*;
 
@@ -323,6 +327,139 @@ use super::*;
                 ]
             ).await
     }
+
+
+    // Scenarios we need to test for processing missing dependency errors:
+    // -> have some of our targets in previously seen
+    // -> buildozer failed
+    // -> print deps say we already have the action
+    // -> for one target, we have multiple class errors. For some of those errors we should share the first hit.
     
+    #[tokio::test]
+    async fn test_process_missing_dependency_errors() {
+
+        // this is a simple scenario, nothing is in the index table, and we have our buildozer set to allow ~everything to pass through
+
+        let buildozer = FakeBuildozer{
+            action_log: Arc::new(Mutex::new(Vec::default()))
+        };
+
+        let content = "src/main/scala/com/example/Example.scala:2: error: object foo is not a member of package com.example
+        import com.example.foo.bar.Baz
+                           ^
+        src/main/scala/com/example/Example.scala:2: warning: Unused import
+        import com.example.foo.bar.Baz
+                                   ^
+        one warning found
+        one error found";
+
+        let mut tempfile = tempfile::NamedTempFile::new().expect("Can make a temp file");
+        tempfile.write_all(content.as_bytes()).expect("Should be able to write to temp file");
+        let tempfile_path = tempfile.into_temp_path();
+
+        let action_failed_error_info = ActionFailedErrorInfo {
+            label: String::from("//src/main/com/example/foo:Bar"),
+            output_files: vec![
+                build_event_stream::file::File::Uri(String::from("remote_uri://foo/bar/baz")),
+                build_event_stream::file::File::Uri(format!("file://{}", &(*tempfile_path).to_path_buf().to_str().unwrap().to_string()))
+            ],
+            target_kind: Some(String::from("scala_library"))
+        };
+
+        let index_table = index_table::IndexTable::default();
+        let global_previous_seen = DashSet::new();
+
+
+        let current_dir = std::env::current_dir().unwrap().to_owned();
+        
+        let working_bazel_tempdir = tempfile::tempdir().expect("Can create tempdir");
+        
+        std::env::set_current_dir(&working_bazel_tempdir.path()).expect("Can set the cwd");
+
+
+        // Now we need to setup the state on the disk such that things will work...
+
+        std::fs::create_dir_all(Path::new("src/main/scala/com/example/foo/bar")).unwrap();
+        std::fs::write("src/main/scala/com/example/foo/bar/BUILD", "java_librar(...)").expect("Should be able to write file");
+
+        std::fs::create_dir_all(Path::new("src/main/scala/com/example/foo")).unwrap();
+        std::fs::write("src/main/scala/com/example/foo/BUILD", "java_librar(...)").expect("Should be able to write file");
+
+
+        let actions = process_missing_dependency_errors(
+            &global_previous_seen,
+            buildozer.clone(),
+            &action_failed_error_info,
+            &index_table,
+        ).await;
+
+        std::env::set_current_dir(&current_dir).expect("Can set the cwd");
+
+        assert_eq!(actions, 1);
+
+        let event_log: Vec<ActionLogEntry> = buildozer.to_vec().await;
+
+        let expected_action_log: Vec<ActionLogEntry> = vec![
+            ActionLogEntry::AddDependency { target_to_operate_on: String::from("//src/main/com/example/foo:Bar"), label_to_add: String::from("//src/main/scala/com/example/foo:foo") }
+        ];
+        assert_eq!(
+            event_log,
+            expected_action_log
+        );
+
+    }
+    #[derive(Clone, Debug, PartialEq)]
+    enum ActionLogEntry {
+        AddDependency{
+            target_to_operate_on: String,
+            label_to_add: String
+        },
+        RemovedDependency{
+            target_to_operate_on: String,
+            label_to_add: String
+        }
+    }
+    #[derive(Clone, Debug)]
+    struct FakeBuildozer {
+        action_log: Arc<Mutex<Vec<ActionLogEntry>>>
+    }
+    impl FakeBuildozer {
+        pub async fn to_vec(&self) -> Vec<ActionLogEntry> {
+            let locked = self.action_log.lock().await;
+            (*locked).clone()
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl Buildozer for FakeBuildozer {
+        async fn print_deps(&self, label: &String) -> Result<Vec<String>, ExecuteResultError> {
+            Ok(Vec::default())
+        }
+        async fn add_dependency(
+            &self,
+            target_to_operate_on: &String,
+            label_to_add: &String,
+        ) -> Result<(), ExecuteResultError> {
+            let mut lock = self.action_log.lock().await;
+            lock.push(ActionLogEntry::AddDependency{
+                target_to_operate_on: target_to_operate_on.clone(),
+                label_to_add: label_to_add.clone(),
+            });
+            Ok(())
+        }
+    
+        async fn remove_dependency(
+            &self,
+            target_to_operate_on: &String,
+            label_to_add: &String,
+        ) -> Result<(), ExecuteResultError> {
+            let mut lock = self.action_log.lock().await;
+            lock.push(ActionLogEntry::RemovedDependency{
+                target_to_operate_on: target_to_operate_on.clone(),
+                label_to_add: label_to_add.clone(),
+            });
+            Ok(())
+        }
+    }
     
 }
