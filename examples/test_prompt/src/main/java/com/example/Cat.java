dff4f6bbe26215a2699495bf647f07114076963a
Ian O'Connell <ianoc@ianoc.net>
2021-05-14 21:54:48 -0600

    cp

--- examples/test_prompt/src/main/java/com/example/Cat.java
+++ examples/test_prompt/src/main/java/com/example/Cat.java
@@ -4,23 +4,3 @@ package com.example;
 public class Cat {
     public String name = "Furry";
 }
-asdf
-
-asd
-asdf
-
-
-
-asd
-asdf
-asdf
-sdfasd
-asdsdfsdfds
-sdfasdasd
-asdfasdfasd
-
-asdfasdf
-asd
-
-asdfasdf
-asdasd
