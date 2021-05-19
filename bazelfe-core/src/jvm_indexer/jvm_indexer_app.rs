ffb9e3ca466e8e8b77c9c8660ced9ae1daf6f7c2
Ian O'Connell <ianoc@ianoc.net>
2021-05-19 11:01:29 -0600

    cp

--- bazelfe-core/src/jvm_indexer/jvm_indexer_app.rs
+++ bazelfe-core/src/jvm_indexer/jvm_indexer_app.rs
@@ -223,12 +223,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     eprintln!("Arg parsing from bazelfe doesn't understand the args, missing an option to {}", o);
                     std::process::exit(-1);
                 }
-            bazelfe_core::bazel_command_line_parser::CommandLineParsingError::UnknownArgument(o) => {
-                eprintln!("Arg parsing from bazelfe doesn't understand the args, unknown option {}", o);
-                std::process::exit(-1);
-        }
-        
-}
+                bazelfe_core::bazel_command_line_parser::CommandLineParsingError::UnknownArgument(o) => {
+                    eprintln!("Arg parsing from bazelfe doesn't understand the args, unknown option {}", o);
+                    std::process::exit(-1);
+                }
+            }
         }
     };
 
