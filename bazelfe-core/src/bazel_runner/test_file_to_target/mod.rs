f4997541b6a72e518d5d8a5935ec3c3779066cab
Ian O'Connell <ianoc@ianoc.net>
2022-04-18 17:34:15 -0400

    Add in the rule to say test_file and give a source file, bazelfe will then rewrite it to the rule that owns that file (#344)

--- bazelfe-core/src/bazel_runner/test_file_to_target/mod.rs
+++ bazelfe-core/src/bazel_runner/test_file_to_target/mod.rs
@@ -1,23 +1,35 @@
 use std::{collections::HashSet, path::PathBuf};
 
-use crate::{bazel_command_line_parser::ParsedCommandLine, jvm_indexer::bazel_query::BazelQuery};
+use crate::{
+    bazel_command_line_parser::{BuiltInAction, ParsedCommandLine},
+    jvm_indexer::bazel_query::BazelQuery,
+};
 
 use super::command_line_rewriter_action::RewriteCommandLineError;
 
-pub async fn rewrite_test_command(
-    command_line: &mut ParsedCommandLine,
-) -> Result<bool, RewriteCommandLineError> {
+fn err(e_string: String) -> Result<(), RewriteCommandLineError> {
+    Err(RewriteCommandLineError::UserErrorReport(
+        super::UserReportError(e_string),
+    ))
+}
+pub async fn run(command_line: &mut ParsedCommandLine) -> Result<(), RewriteCommandLineError> {
     let mut on_disk_files = Vec::default();
     for command_line_opts in command_line.remaining_args.iter() {
         let pb = PathBuf::from(command_line_opts);
         if !pb.exists() {
-            return Ok(false);
+            return err(format!(
+                "Path specified {}, does not exist",
+                pb.to_string_lossy()
+            ));
+        }
+        if !pb.is_relative() {
+            return err(format!("Path specified {}, is not relative to the repo, absolute paths are not yet supported.", pb.to_string_lossy()));
         }
         on_disk_files.push(pb);
     }
 
     if on_disk_files.is_empty() {
-        return Ok(false);
+        return err(format!("No files on disk specified to run"));
     }
 
     let bazel_query = crate::jvm_indexer::bazel_query::from_binary_path(&command_line.bazel_binary);
@@ -35,11 +47,10 @@ pub async fn rewrite_test_command(
         let root_with_build = if let Some(root_with_build) = cur_parent {
             root_with_build.to_path_buf()
         } else {
-            eprintln!(
+            return err(format!(
                 "Couldn't find a parent with a BUILD or BUILD.bazel in any parent of {}",
                 p.to_string_lossy()
-            );
-            return Ok(false);
+            ));
         };
 
         let query_for_target = bazel_query
@@ -53,8 +64,7 @@ pub async fn rewrite_test_command(
             ])
             .await;
         if query_for_target.exit_code != 0 {
-            eprintln!("Attempted to query for target that owns the file {}, but bazel query returned error:\n{}", p.to_string_lossy(), query_for_target.stderr);
-            return Ok(false);
+            return err(format!("Attempted to query for target that owns the file {}, but bazel query returned error:\n{}", p.to_string_lossy(), query_for_target.stderr));
         }
 
         if let Some(ln) = query_for_target.stdout.lines().nth(1) {
@@ -63,20 +73,17 @@ pub async fn rewrite_test_command(
     }
 
     if targets.is_empty() {
-        eprintln!(
+        return err(format!(
             "Attempted to query for targets built failed to find any, looked for files:\n{}",
             command_line.remaining_args.join(",")
-        );
-        return Ok(false);
+        ));
     }
 
-    eprintln!(
-        "Rewritten source files: {}",
-        command_line.remaining_args.join(",")
-    );
     command_line.remaining_args.clear();
     command_line.remaining_args.extend(targets.into_iter());
-    eprintln!("To targets: {}", command_line.remaining_args.join(","));
 
-    Ok(false)
+    command_line.action = Some(crate::bazel_command_line_parser::Action::BuiltIn(
+        BuiltInAction::Test,
+    ));
+    Ok(())
 }
