e1ab53bc675f0fe962d600757c771b12a454777e
Ian O Connell <ianoc@ianoc.net>
2022-09-21 17:43:20 -0600

    cp

--- bazelfe-core/src/bep_junit/test_results_ops.rs
+++ bazelfe-core/src/bep_junit/test_results_ops.rs
@@ -36,7 +36,7 @@ pub fn emit_backup_error_data(test_result: &TestResultInfo, output_root: &Path) 
     // we have ran into issues with non-utf-8 characters in the output logs
     // so we will replace anything not-ascii to '?' cut it right back
     let output_data = extract_file_content(test_result).join("\n")
-        .replace(|c: char| !c.is_ascii() || c.is_ascii_control() || c.is_ascii_graphic(), "?");
+        .replace(|c: char| !c.is_ascii() || (c != '\n' && c != '\r' && c.is_ascii_control()), "?");
 
     let known_failures = vec![junit_xml_error_writer::Failure {
         message: format!("Test aborted, {}", label_name),
